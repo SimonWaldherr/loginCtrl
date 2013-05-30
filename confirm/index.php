e4c3e68b3eddfb16ee9b625f83c584d8352ab12f
Simon Waldherr <git@simon.waldherr.eu>
2013-05-30 16:05:42 +0200

    cleanup for releases

--- confirm/index.php
+++
@@ -1,2 +0,0 @@
-You must select a mode (MySQL or SQLite), to use this login script.  
-To do this, you have to delete this file and rename mysql.php or sqlite.php to index.php  
--- confirm/readme.txt
+++
@@ -1,2 +0,0 @@
-You must select a mode (MySQL or SQLite), to use this login script.  
-To do this, you have to rename mysql.php or sqlite.php to index.php  
